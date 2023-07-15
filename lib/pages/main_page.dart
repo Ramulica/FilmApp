b91d308924711658d7a0e344f6a94c55331cf5b3
Ramulica <andrei.ciprian.r@gmail.com>
2023-07-15 23:33:10 +0300

    pregatire de plecare la mare

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -4,6 +4,8 @@ import 'package:newsapp/globals.dart' as globals;
 import 'package:newsapp/widgets/film_card.dart';
 import 'package:newsapp/models/sql_movie.dart';
 import 'package:newsapp/widgets/text_fileld.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
 
@@ -35,6 +37,14 @@ class MainPage extends StatelessWidget {
             tooltip: 'Search Movie',
             onPressed: () {
               Navigator.of(context).pushNamed("SearchMovie");
+            },
+          ),
+           IconButton(
+            icon: const Icon(Icons.safety_check),
+            tooltip: 'Search Movie',
+            onPressed: () async {
+              await Firebase.initializeApp();
+              await FirebaseFirestore.instance.collection("films/JypfxOanE7RlbViUS4KO/Movies").snapshots().listen((event) {print(event.docs[0]["text"]);});
             },
           )
           ],
